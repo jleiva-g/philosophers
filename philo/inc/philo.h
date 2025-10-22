8e50b7dc60fbd373f097dcd899dec99a33401873
Jesus Leiva Guerrero <jleiva-g@c2r1s4.42malaga.com>
2025-10-22 16:43:26 +0200

    initial commit

---
+++ philo/inc/philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jleiva-g <jleiva-g@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/22 16:07:29 by jleiva-g          #+#    #+#             */
+/*   Updated: 2025/10/22 16:41:09 by jleiva-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
