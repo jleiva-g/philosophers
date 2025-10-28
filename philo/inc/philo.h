e913e09779f0f41b3ce1925bb7ff05fdb92db77d
Jesus Leiva Guerrero <jleiva-g@c2r6s1.42malaga.com>
2025-10-28 19:39:17 +0100

    get_time

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: jleiva-g <jleiva-g@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 16:07:29 by jleiva-g          #+#    #+#             */
-/*   Updated: 2025/10/22 16:41:09 by jleiva-g         ###   ########.fr       */
+/*   Updated: 2025/10/28 19:36:46 by jleiva-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,24 @@
 # include <unistd.h>
 # include <sys/time.h>
 # include <pthread.h>
+
+typedef struct s_philo
+{
+	int		index;
+	long	last_eat;
+	long	times_eat;
+}	t_philo;
+
+typedef struct s_data
+{
+	int		nphilos;
+	long	tdie;
+	long	teat;
+	long	tsleep;
+	long	must_eat;
+}	t_data;
+
+// utils
+long	get_time(long time);
 
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: jleiva-g <jleiva-g@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 16:06:25 by jleiva-g          #+#    #+#             */
-/*   Updated: 2025/10/22 16:41:58 by jleiva-g         ###   ########.fr       */
+/*   Updated: 2025/10/28 19:18:16 by jleiva-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,17 @@
 
 int	main(int argc, char **argv)
 {
+	int				i;
+	long			time;
+
 	(void) argc;
 	(void) argv;
-	printf("hola\n");
+	i = 10;
+	time = get_time(0);
+	while (i--)
+	{
+		usleep(1000000);
+		printf("%08li\n", get_time(time));
+	}
 	return (0);
 }
