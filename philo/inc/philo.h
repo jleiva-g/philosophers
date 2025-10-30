253efe39106a64a54bcfe6fe06676dfef0fbf76b
Jesus Leiva Guerrero <jleiva-g@c1r15s4.42malaga.com>
2025-10-30 18:36:37 +0100

    working on data struct

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: jleiva-g <jleiva-g@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 16:07:29 by jleiva-g          #+#    #+#             */
-/*   Updated: 2025/10/28 20:01:56 by jleiva-g         ###   ########.fr       */
+/*   Updated: 2025/10/30 16:53:38 by jleiva-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,29 @@
 # include <sys/time.h>
 # include <pthread.h>
 
-typedef struct s_philo
-{
-	int		index;
-	long	last_eat;
-	long	times_eat;
-}	t_philo;
-
 typedef struct s_data
 {
-	int		nphilos;
-	long	tdie;
-	long	teat;
-	long	tsleep;
-	long	must_eat;
+	int				nphilos;
+	long			tdie;
+	long			teat;
+	long			tsleep;
+	long			max_meals;
+	int				status;
+	pthread_t		*philos;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	*mutex;
 }	t_data;
+
+typedef struct s_philo
+{
+	int				id;
+	struct s_data	data;
+	long			last_meal;
+	long			meals;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+	pthread_mutex_t	*mutex;
+}	t_philo;
 
 // utils
 long	get_time(long time);
