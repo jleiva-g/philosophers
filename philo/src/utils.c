731a1208c650d548a2bd7369e2ad3348d49ecbf9
jleiva-g <jesus@DESKTOP-LH7SKOE.localdomain>
2025-11-07 17:03:49 +0100

    added colors

--- philo/src/utils.c
+++ philo/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: jleiva-g <jleiva-g@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 10:33:04 by jleiva-g          #+#    #+#             */
-/*   Updated: 2025/11/04 14:21:41 by jleiva-g         ###   ########.fr       */
+/*   Updated: 2025/11/07 17:03:12 by jleiva-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,16 +39,16 @@ void	print_status(t_table *table, int philo, char mode)
 
 	str = "";
 	if (mode == 'f')
-		str = "has taken a fork";
+		str = "\e[33m\0has taken a fork\e[0m";
 	else if (mode == 'e')
-		str = "is eating";
+		str = "\e[32m\0is eating\e[0m";
 	else if (mode == 's')
-		str = "is sleeping";
+		str = "\e[34m\0is sleeping\e[0m";
 	else if (mode == 't')
-		str = "is thinking";
+		str = "\e[35m\0is thinking\e[0m";
 	else if (mode == 'd')
-		str = "died";
+		str = "\e[31m\0died\e[0m";
 	pthread_mutex_lock(&table->wmutex);
-	printf("%06li %i %s\n", get_time(table->stime), philo, str);
+	printf("%s%06li %i %s\n", str, get_time(table->stime), philo, str + 6);
 	pthread_mutex_unlock(&table->wmutex);
 }
